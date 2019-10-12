09803b312b56ca1e6f73b2fcd8dfaed9fc7afb48
Koke Gudiel <kokegudiel2@gmail.com>
2019-10-12 16:52:38 -0600

    Correccion de Requiered

--- CrudCore/Models/TbPersonajes.cs
+++ CrudCore/Models/TbPersonajes.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CrudCore.Models
 {
     public partial class TbPersonajes
     {
         public int IdPersonaje { get; set; }
+        [Required]
         public string NombrePersonaje { get; set; }
+        [Range(typeof(DateTime), "1938-01-01", "1959-12-31",
+                    ErrorMessage = "La Fecha debe estar entre el 1 de enero de 1938 hasta el 31 de diciembre del 1951")]
         public DateTime FechaPersonaje { get; set; }
+        [Range(1, 100)]
         public int EdadPersonaje { get; set; }
         public string EditorialPersonaje { get; set; }
     }
